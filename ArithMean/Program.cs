645f18967b212ca883c20831890e17fd6a2fda7b
Katerina <ealmazurova@internet.ru>
2022-04-02 14:21:50 +0300

    Задача на вывод среднеарифметического столбцов массива

---
+++ ArithMean/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 3: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(double[] collect)
+{
+    Console.Write("[");
+    for (int i = 0; i < collect.Length - 1; i++)
+    {
+        Console.Write($"{collect[i]:0.00}, ");
+
+    }
+    Console.Write($"{collect[collect.Length - 1]:0.00}]");
+}
+
+double[] ArithmeticMean(int[,] array)
+{
+    double[] arr = new double[array.GetLength(1)];
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        arr[j] = sum / array.GetLength(0);
+        sum = 0;
+    }
+    return arr;
+}
+
+int[,] array = FillArray(4, 4);
+PrintArray2(array);
+Console.WriteLine();
+double[] arr = ArithmeticMean(array);
+PrintArray(arr);
